f644577a89c95ac72cbb7f9b7ac20f6786271015
jtithy <tithyjebunnesa202@gmail.com>
2026-01-22 07:40:30 +0600

    Softmax converts outputs to probabilities

--- New.py
+++ New.py
@@ -34,18 +34,23 @@ for intent in intents['intents']:
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
             
-
+# Lemmatize words and remove ignored characters
 words = [lemmatizer.lemmatize(word) for word in words if word not in ignoreLetters]
-words = sorted(set(words))
 
+# Remove duplicates and sort
+words = sorted(set(words))
 classes = sorted(set(classes))
 
+# Save processed words and classes
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
 
+# Prepare training data
 training = []
+# Output template for one-hot encoding
 outputEmpty = [0] * len(classes)
 
+# Create bag-of-words and output vectors
 for document in documents:
     bag = []
     wordPatterns = document[0]
@@ -55,11 +60,33 @@ for document in documents:
     outputRow = list(outputEmpty)
     outputRow[classes.index(document[1])] = 1
     training.append(bag + outputRow)
+
+# Shuffle training data
 random.shuffle(training)
 training = np.array(training)
 
+# Split input (X) and output (Y)
 trainX = training[:, :len(words)]
 trainY = training[:, len(words):]
 
+
+# Initialize neural network model
 model = tf.keras.Sequential()
-    
+
+# input_shape defines the number of features (bag-of-words size)
+model.add(tf.keras.layers.Dense(
+    128,
+    input_shape=(len(trainX[0]),),
+    activation='relu'
+))
+model.add(tf.keras.leyars.Dropout(0.5))
+
+# Second hidden layer with 64 neurons
+model.add(tf.keras.leyars.Dense(
+    64, 
+    activition = 'relu'))
+
+# Softmax converts outputs to probabilities
+model.add(tf.keras.leyars.Dense(
+    len(trainY[0]), 
+    activation = 'softmax'))
